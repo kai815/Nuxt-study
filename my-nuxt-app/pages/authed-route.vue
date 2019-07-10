1def38f80dd07170481115e9f468b391deb0261e
kai815 <h.kaizuka815@gmail.com>
2019-07-10 22:10:23 +0900

    modify nuxtconfig auth view

--- my-nuxt-app/pages/authed-route.vue
+++ my-nuxt-app/pages/authed-route.vue
@@ -8,16 +8,3 @@
         </p>
     </div>
 </template>
-
-<script>
-import Cookies from 'universal-cookie'
-export default {
-    methods: {
-        login() {
-            const cookies = new Cookies()
-            cookies.set('credential', 'true', {maxAge: 90})
-            this.$router.push('/')
-        }
-    }
-}
-</script>
--- my-nuxt-app/pages/login.vue
+++ my-nuxt-app/pages/login.vue
@@ -7,3 +7,15 @@
         </p>
     </div>
 </template>
+<script>
+import Cookies from 'universal-cookie'
+export default {
+    methods: {
+        login() {
+            const cookies = new Cookies()
+            cookies.set('credential', 'true', {maxAge: 90})
+            this.$router.push('/')
+        }
+    }
+}
+</script>
